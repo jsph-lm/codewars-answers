3e6dadb8cdeb845211005f45df8b44d969c53699
jsph-lm <jsphlm@Josephs-MacBook-Air.local>
2023-02-19 17:28:27 +0800

    add finished codewars challenges 19Feb2023

---
+++ 8kyu/add-item-to-array.js
@@ -0,0 +1,16 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+//my solution
+// add the value "codewars" to the already defined websites array
+var websites;
+websites.push("codewars");
+
+
+// add the value "codewars" to the already defined websites array
+websites.push('codewars')
+
